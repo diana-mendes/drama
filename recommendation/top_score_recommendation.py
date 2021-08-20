e258ebf127e0187a376d5c66cb25dc9526d8a224
diana-mendes <34720262+diana-mendes@users.noreply.github.com>
2021-08-20 14:40:23 +0100

    Add parent directory to sys path

--- recommendation/top_score_recommendation.py
+++ recommendation/top_score_recommendation.py
@@ -1,5 +1,8 @@
+import sys
+import os
 import pandas as pd
 
+sys.path.append("..")
 DATA_PATH = 'data/clean_data_files/clean_fields.csv'
 
 NAME_COL = 'main_name'
@@ -16,7 +19,8 @@ def _load_data():
 	Loads data for recommendation from CSV files.
 	:return: pd.DataFrame with CSV_COLUMNS_TO_KEEP for all data found
 	"""
-	df = pd.read_csv(DATA_PATH, sep='\t')[CSV_COLUMNS_TO_KEEP]
+	data_path = os.path.join(os.path.abspath('..'), DATA_PATH)
+	df = pd.read_csv(data_path, sep='\t')[CSV_COLUMNS_TO_KEEP]
 	_convert_data_types(df)
 	return df
 
@@ -27,7 +31,7 @@ def _convert_data_types(df):
 	df[NUMBER_REVIEWS_COL] = df[NUMBER_REVIEWS_COL].astype(int)
 
 
-def _calculate_weighted_score(df):
+def _calculate_review_weighted_score(df):
 	df['weight_factor'] = df.apply(lambda x: min(5, x[NUMBER_REVIEWS_COL])/5, axis=1)
 	df[WEIGHTED_SCORE_COL] = df[REVIEW_SCORE_COL] * df['weight_factor']
 	df.drop('weight_factor', axis=1, inplace=True)
@@ -44,14 +48,14 @@ class TopScoreRecommendation:
 		else:
 			return None
 
-	def get_top_recos_by_weighted_score(self, input_drama, k=3):
+	def get_top_recos_by_review_weighted_score(self, input_drama, k=3):
 		genre = self._get_input_genre(input_drama)
 		if genre is None:
 			return []
 		genre_candidates = self.data[(self.data[GENRE_COL] == genre) & (self.data[NAME_COL] != input_drama)].copy()
 		if len(genre_candidates) == 0:
 			return []
-		_calculate_weighted_score(genre_candidates)
+		_calculate_review_weighted_score(genre_candidates)
 		recommended = genre_candidates.sort_values(by=WEIGHTED_SCORE_COL, ascending=False).head(k)
 		print(recommended[CSV_COLUMNS_TO_KEEP])
 		return recommended[NAME_COL].tolist()
--- scripts/get_recommendation.py
+++ scripts/get_recommendation.py
@@ -1,7 +1,10 @@
+import sys
+sys.path.append("..")
+
 from recommendation.top_score_recommendation import TopScoreRecommendation
 
 
 if __name__ == "__main__":
 	request = 'A Place in the Sun'
 	reco = TopScoreRecommendation()
-	print(reco.get_top_recos_by_weighted_score(request, 3))
+	print(reco.get_top_recos_by_review_weighted_score(request, 3))
