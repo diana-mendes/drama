baf4a22e65ed674bbb6b82a0633baebd48c72276
Diana Mendes <diana.rmendes@gmail.com>
2021-08-20 17:51:12 +0100

    switch website recommendation from review score to genre similarity

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 from recommendation.top_score_recommendation import TopScoreRecommendation
+from recommendation.genre_recommendation import GenreRecommendation
 
 app = Flask(__name__)
 
@@ -10,8 +11,11 @@ def recommend(request):
 	:param request: string with input drama, e.g.: 'A Place in the Sun'.
 	:return: list of recommended drama names
 	"""
-	recommender = TopScoreRecommendation()
-	drama_list = recommender.get_top_recos_by_review_weighted_score(request, 3)
+	# score_recommender = TopScoreRecommendation()
+	# drama_list = score_recommender.get_top_recos_by_review_weighted_score(request, 3)
+	
+	genre_recommender = GenreRecommendation()
+	drama_list = genre_recommender.get_genre_recommendation(request, 3)
 	return drama_list
 
 
